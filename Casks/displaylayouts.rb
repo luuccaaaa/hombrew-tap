a737aca87440767446051251296f2ca2ed84ebf1
Luca <125209471+luuccaaaa@users.noreply.github.com>
2025-10-04 12:16:29 +0200

    Fix URL and homepage for displaylayouts cask

--- Casks/displaylayouts.rb
+++ Casks/displaylayouts.rb
@@ -1,12 +1,12 @@
- cask "displaylayouts" do
+    cask "displaylayouts" do
     version "1.0"
     sha256 "e0942d6bfa58d6fe0fe4ce4a0c5f2f16001d00c209bbe9f6122373b348899f13"
 
-    url "https://github.com/luuccaaaa/DisplayLayoutsApp/releases/download/v#{version}/DisplayLayouts-#{version}.dmg",
-    verified: "github.com/luuccaaaa/DisplayLayoutsApp/"
+    url "https://github.com/luuccaaaa/DisplayLayouts/releases/download/v#{version}/DisplayLayouts-#{version}.dmg",
+    verified: "github.com/luuccaaaa/DisplayLayouts/"
     name "DisplayLayouts"
     desc "Menu bar app to switch multi-monitor layouts"
-    homepage "https://github.com/luuccaaaa/DisplayLayoutsApp"
+    homepage "https://github.com/luuccaaaa/DisplayLayouts"
 
     livecheck do
     url :url
